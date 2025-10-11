86bcc119409146fe74fc2055468d62a0b4b96062
9jh1 <tkf.x1os@gmail.com>
2025-10-11 16:27:05 +1300

    Sat Oct 11 04:27:05 PM NZDT 2025

--- main.c
+++ main.c
@@ -1,40 +1,37 @@
 #include "include.h"
 
-
 auth_config opt;
 toml_result_t config_file;
-
 
 int main(const int c, const char *v[]) {
   pl_arg *help_arg = PL_P("--help", "show this dialog");
   pl_arg *arg_config_paths = PL_P("--config-path", "show config path");
-	pl_arg *arg_config_path = PL_P("--config","load a config file",.takes_value = 1);
+  pl_arg *arg_config_path =
+      PL_P("--config", "load a config file", .takes_value = 1);
   pl_r ret;
 
-	// parse plib arguments
-  if ((ret = pl_proc(c, v)) == PL_SUCCESS){
-		printf("help.triggered = %d\n",help_arg->triggered);
+  // parse plib arguments
+  if ((ret = pl_proc(c, v)) == PL_SUCCESS) {
+    printf("help.triggered = %d\n", help_arg->triggered);
 
-	// if --help 
-	if (PL_R(help_arg)){
-		printf("--help called\n");
-		goto help_exit;
-	}
+    // if --help
+    if (PL_R(help_arg)) {
+      printf("--help called\n");
+      goto help_exit;
+    }
+
+    // if config-path
+    if (PL_R(arg_config_paths)) {
+      printf("Default config path is: \"%s\"\n", DEFAULT_CONFIG_PATH);
+      quit(SUCCESS);
+    }
+  }
 
 
-	// if config-path
-	if(PL_R(arg_config_paths)){
-		printf("Default config path is: \"%s\"\n",DEFAULT_CONFIG_PATH);
-		quit(SUCCESS);
-	}
-	}
+  quit(SUCCESS);
 
-	// get the config file 
-
-	quit(SUCCESS);
-	
-	// PLib error handling
-	help_exit:
+// PLib error handling
+help_exit:
   help();
 
   if (ret != PL_SUCCESS) {
